9a03fb84a10a2665aca469291ffb19db7bcd03ce
hhinkel <hhinkel15@gmail.com>
2024-11-21 10:05:55 -0600

    Event table

--- scripts/events.js
+++ scripts/events.js
@@ -1,5 +1,39 @@
 const events = document.getElementById("events");
 
+function createVolunteerCell(item, row) {
+    const cell = document.createElement('td');
+    cell.textContent = "Volunteer Button";
+    row.appendChild(cell);
+}
+
+function createCell(data, row) {
+    const cell = document.createElement('td');
+    cell.textContent = data;
+    row.appendChild(cell);
+}
+
+function createEventTable(eventItems) {
+    const numCells = 4;
+    const tbl = document.createElement('table');
+    const tblbody = document.createElement('tbody');
+    //create rows
+    for (const eventItem of eventItems) {
+        const date = new Date(eventItem.date);
+        const now = new Date();
+        if (date < now)
+            continue;
+        const row = document.createElement('tr');
+        // create cells
+        createCell(date.toDateString(), row);
+        createCell(date.toLocaleTimeString('en-US'), row);
+        createCell(eventItem.text, row);
+        createVolunteerCell(eventItem, row);
+        tblbody.appendChild(row);
+    }
+    tbl.appendChild(tblbody);
+    events.appendChild(tbl).className = "eventList";
+}
+
 function fetchEventData() {
     fetch("/./events.json") 
         .then((res) => {
@@ -8,23 +42,9 @@ function fetchEventData() {
             }
             return res.json();
         })
-        .then((data) =>
-            createEventList(data.events))
+        .then((data) => 
+            createEventTable(data.events))
         .catch((error) => console.error("Unable to fetch data:", error));
-}
-
-function createEventList(eventItems) {
-    const ul = document.createElement("ul");
-    for (const eventItem of eventItems) {
-        const date = new Date(eventItem.date);
-        const now = new Date();
-        if (date < now)
-            continue;
-        const li = document.createElement("li");
-        li.textContent = date.toDateString() + ": " + eventItem.text;
-        ul.appendChild(li).className = "event_list_items";
-    }
-    events.appendChild(ul).className = "eventList";
 }
 
 fetchEventData();
