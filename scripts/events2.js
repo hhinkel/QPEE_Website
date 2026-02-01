02de9d6269df1785776d926f9dccdf9d1aa7ffea
hhinkel <hhinkel15@gmail.com>
2026-02-01 05:50:47 -0600

    Simple changes

--- scripts/events2.js
+++ scripts/events2.js
@@ -1,10 +1,12 @@
 const events = document.getElementById("events2");
-let test;
+let eventData = [];
 
 function createEvents() {
     const test = fetchEvents();
     // parse event file
     // setup table 4 cells (date, time, description, link) x rows
+    console.log("B", eventData);
+    console.log("C", test);
     const p = document.createElement("p");
     const event = '{"date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None"}';
     console.log(event);
@@ -15,19 +17,23 @@ function createEvents() {
 }
 
 
-function fetchEvents() {
-    fetch('./events.json')
-        .then((res) => {
-            if(!res.ok) {
-                throw new Error ('HTTP error! Status: ${res.status}');
-            }
-        })
-        .then(events2 => {
-            test = events2;
-            console.log(test);
-        })
-        .catch((error) => {
-            console.error("Unable to fetch data:", error);
-})}
+async function fetchEvents() {
+    try {
+        const res = await fetch('./events.json');
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        eventData = await res.json();
+        console.log(eventData.events);
+        } catch (error) {
+            console.error('Failed to load JSON data:', error);
+        }
+        return eventData.events;
+    }
 
 createEvents();
+
+
+/* .then(res => res.json())    
+            .then((data) => data.events)
+            .catch(error => console.error("Unable to fetch data:", error)); */
