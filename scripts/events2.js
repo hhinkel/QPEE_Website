8e1e3ecbc82632a4ad7aea7a1a4af32a2e680db6
hhinkel <hhinkel15@gmail.com>
2026-02-25 15:56:20 -0600

    Calender changes

--- scripts/events2.js
+++ scripts/events2.js
@@ -1,39 +1,24 @@
-const events = document.getElementById("events2");
-let eventData = [];
+const events = [
+    '{ "date": "2026-04-11 09:00", "text": "Work Day", "link": "None" }',
+    '{ "date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None" }'
+]
 
 function createEvents() {
-    const test = fetchEvents();
     // parse event file
     // setup table 4 cells (date, time, description, link) x rows
-    console.log("B", eventData);
-    console.log("C", test);
+    console.log("events", events);
     const p = document.createElement("p");
-    const event = '{"date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None"}';
-    console.log(event);
-    const objEvent = JSON.parse(event);
-    console.log(objEvent["date"]);
-    p.textContent = "Date: " + objEvent.date + " Description: " + objEvent.text;
-    events2.appendChild(p).className = "eventList";
+    const br = document.createElement("br");
+    //const event = '{"date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None"}';
+    for (let i= 0; i < events.length; i++) {
+        const event =events[i];
+        console.log("event: "+ event);
+        const objEvent = JSON.parse(event);
+        console.log(objEvent);
+        p.textContent = "Date: " + objEvent["date"] + " Description: " + objEvent["text"];
+        br.textContent;
+        events2.appendChild(p).className = "eventList";
+    }
 }
 
-
-async function fetchEvents() {
-    try {
-        const res = await fetch('./events.json');
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        eventData = await res.json();
-        console.log(eventData.events);
-        } catch (error) {
-            console.error('Failed to load JSON data:', error);
-        }
-        return eventData.events;
-    }
-
 createEvents();
-
-
-/* .then(res => res.json())    
-            .then((data) => data.events)
-            .catch(error => console.error("Unable to fetch data:", error)); */
