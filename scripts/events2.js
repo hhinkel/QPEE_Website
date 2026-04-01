679282a054f44f8027638ab51348e8a035aa10da
hhinkel <hhinkel15@gmail.com>
2026-04-01 09:37:10 -0500

    Changes to calendar page

--- scripts/events2.js
+++ scripts/events2.js
@@ -1,23 +1,38 @@
 const events = [
-    '{ "date": "2026-04-11 09:00", "text": "Work Day", "link": "None" }',
-    '{ "date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None" }'
+    { "date": "2026-04-04 00:00", "text": "Closing for Fix-a-Test and Mini Event", "link": "None"},
+    { "date": "2026-04-11 09:00", "text": "Fix-a-Test", "link": "None" },
+    { "date": "2026-04-12 08:00", "text": "Mini Event", "link": "None" }
 ]
+const body = document.getElementById("body");
 
 function createEvents() {
-    // parse event file
-    // setup table 4 cells (date, time, description, link) x rows
     console.log("events", events);
-    const p = document.createElement("p");
-    //const event = '{"date": "2026-04-12 08:00", "text": "Schooling Day and Ride a Test", "link": "None"}';
-    for (let i= 0; i < events.length; i++) {
-        const event =events[i];
-        console.log("event: "+ event);
-        const objEvent = JSON.parse(event);
-        console.log(objEvent);
-        p.textContent = "Date: " + objEvent["date"] + " Description: " + objEvent["text"];
-        events2.appendChild(p).className = "eventList";
-        events2.innerHTML += "<p></p>"; 
-    }
+    const table = document.createElement("table");
+    table.border = 1;
+
+    //Create Headers
+    const headers = ["Date/Time", "Event", "Link"];
+    const headerRow = document.createElement("tr");
+    headers.forEach(text => {
+        const th = document.createElement("th");
+        th.textContent = text;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // Create Data Rows
+    events.forEach(event => {
+        const row = document.createElement("tr");
+        Object.values(event).forEach(value => {
+            const cell = document.createElement("td");
+            cell.textContent = value;
+            row.appendChild(cell);
+        });
+        table.appendChild(row);
+    });
+
+    // Insert table
+    document.getElementById("table-container").appendChild(table);
 }
 
 createEvents();
