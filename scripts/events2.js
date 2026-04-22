f8a05644c4efc430ef7eeef9ea0d7bb90e69ca1c
hhinkel <hhinkel15@gmail.com>
2026-04-21 20:42:17 -0500

    Calendar will not show events that are past

--- scripts/events2.js
+++ scripts/events2.js
@@ -1,26 +1,11 @@
-const events = [
-    { "date": "2026-03-24 00:00", "text": "Opening Date Queeny Park Horse Trials", "link": "https://useventing.com/events-competitions/calendar/event?event=18983", "linkText": "Omnibus" },
-    { "date": "2026-04-11 09:00", "text": "Fix-a-Test (Cancelled)", "link": "None", "linkText": "None" },
-    { "date": "2026-04-12 08:00", "text": "Mini Event (Closed)", "link": "None", "linkText": "None" },
-    { "date": "2026-03-24 00:00", "text": "Closing Date Queeny Park Horse Trials", "link": "https://useventing.com/events-competitions/calendar/event?event=18983", "linkText": "Omnibus" },
-    { "date": "2026-05-02 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-03 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-04 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-05 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-06 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-07 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-07 23:00", "text": "Start Times Avalible", "link": "Link will be live when avalible", "linkText": "None" },
-    { "date": "2026-05-08 08:00", "text": "Work Day for Horse Trials", "link": "None", "linkText": "None" },
-    { "date": "2026-05-09 08:00", "text": "Queeny Park Horse Trials (Dressage + Stadium Day)", "link": "https://useventing.com/events-competitions/calendar/event?event=18983", "linkText": "Omnibus" },
-    { "date": "2026-05-09 08:00", "text": "Queeny Park Horse Trials (Cross Country Day)", "link": "None", "linkText": "None" },
-    { "date": "2026-06-27 20:00", "text": "Trivia Night (Tenative)", "link": "None", "link": "https://useventing.com/events-competitions/calendar/event?event=18983", "linkText": "Omnibus" }
-]
+
+import events from "./events.json" with {type: 'json'};
+
 const body = document.getElementById("body");
 
 function createEvents() {
     const table = document.createElement("table");
     table.style.alignSelf = 'center';
-    //table.style.border = '2px solid black';
 
     //Create Headers
     const headers = ["Date", "Time", "Event", "Link"];
@@ -28,7 +13,6 @@ function createEvents() {
     headers.forEach(text => {
         const th = document.createElement("th");
         th.textContent = text;
-        //th.style.border = '2px solid black';
         headerRow.appendChild(th);
     });
     table.appendChild(headerRow);
@@ -37,27 +21,31 @@ function createEvents() {
 
     function createData() {
         events.forEach(event => {
-            const row = table.insertRow();
-            const eventDate = getDate(event.date);
-            const eventTime = getTime(event.date);
+            const d = new Date(event.date);
+            if (d.getTime() > Date.now()) {
+                const eventDate = getDate(event.date);
+                const today = getToday();
+                const eventTime = getTime(event.date);
 
-            const date = row.insertCell();
-            row.appendChild(createCell(date, eventDate));
-            const time = row.insertCell();
-            row.appendChild(createCell(time, eventTime));
-            const text = row.insertCell();
-            row.appendChild(createCell(text, event.text));
-            const link = row.insertCell();
-            if (event.linkText == "None") {
-                row.appendChild(createCell(link, event.link));
-            } else {
-                const aLink = document.createElement("a");
-                aLink.textContent = event.linkText;
-                aLink.href = event.link;
-                //link.style.border = '2px solid black';
-                link.style.padding = '5px';
-                aLink.target = "_blank";
-                link.appendChild(aLink);
+                const row = table.insertRow();
+                const date = row.insertCell();
+                row.appendChild(createCell(date, eventDate));
+                const time = row.insertCell();
+                row.appendChild(createCell(time, eventTime));
+                const text = row.insertCell();
+                row.appendChild(createCell(text, event.text));
+                const link = row.insertCell();
+                if (event.linkText == "None") {
+                    row.appendChild(createCell(link, event.link));
+                } else {
+                    const aLink = document.createElement("a");
+                    aLink.textContent = event.linkText;
+                    aLink.href = event.link;
+                    //link.style.border = '2px solid black';
+                    link.style.padding = '5px';
+                    aLink.target = "_blank";
+                    link.appendChild(aLink);
+                }
             }
         });
     }
@@ -69,6 +57,14 @@ function createEvents() {
         return month + "/" + day + "/" + year;
     }
 
+    function getToday() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const day = date.getDate();
+        return month + "/" + day + "/" + year;
+    }
+
     function getTime(dateTime) {
         const time = dateTime.slice(11, 16);
         return time;
@@ -76,7 +72,6 @@ function createEvents() {
 
     function createCell(cell, value) {
         cell.textContent = value;
-        //cell.style.border = '2px solid black';
         cell.style.padding = '5px';
         return cell;
     }
